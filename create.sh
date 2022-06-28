48a56172bb7a4bccba0f1fcfc99d5534a4da03d5
Johanna Reiml <johanna.reiml@unibw.de>
2022-06-28 11:29:13 +0200

    fix: Increase RAM in create script

--- create.sh
+++ create.sh
@@ -26,31 +26,31 @@ mvn assembly:single
 cd ..
 
 # Download DBpedia files and create initial kb files
-java -Xmx4G -cp com.ibm.research.ai.ki.kb/target/kb-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx8G -cp com.ibm.research.ai.ki.kb/target/kb-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.kb.conversion.ConvertDBpedia -config $config -kb $baseDir/kb
 
 # Download Common Crawl, get -urlList from http://commoncrawl.org/connect/blog/
-java -Xmx4G -cp com.ibm.research.ai.ki.corpus/target/corpora-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx8G -cp com.ibm.research.ai.ki.corpus/target/corpora-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.corpora.crawl.SaveCommonCrawl -config $config -urlList $warcUrlList -out $baseDir/docs.json.gz.b64
 
 # get node corpus counts by baseline EDL
-java -Xmx8G -cp com.ibm.research.ai.ki.kbp/target/kbp-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx32G -cp com.ibm.research.ai.ki.kbp/target/kbp-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.kbp.GazetteerEDL -gazEntries $baseDir/kb/gazEntries.ser.gz -in $baseDir/docs.json.gz.b64 -idCounts $baseDir/kb/idCounts.tsv
 
 # create remaining KB files
-java -Xmx8G -cp com.ibm.research.ai.ki.kb/target/kb-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx32G -cp com.ibm.research.ai.ki.kb/target/kb-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.kb.conversion.ConvertDBpedia -config $config -kb $baseDir/kb
 
 # annotate corpus with baseline EDL
-java -Xmx8G -cp com.ibm.research.ai.ki.kbp/target/kbp-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx32G -cp com.ibm.research.ai.ki.kbp/target/kbp-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.kbp.GazetteerEDL -gazEntries $baseDir/kb/gazEntriesFiltered.ser.gz -in $baseDir/docs.json.gz.b64 -out $baseDir/docs-gaz.json.gz.b64
 
 # baseline context set construction
-java -Xmx8G -cp com.ibm.research.ai.ki.kbp/target/kbp-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx32G -cp com.ibm.research.ai.ki.kbp/target/kbp-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.kbp.KBPBuildDataset -config $config -in $baseDir/docs-gaz.json.gz.b64 -out $baseDir/dataset -kb $baseDir/kb
 
 # dataset processing for seq2seq task
-java -Xmx8G -cp com.ibm.research.ai.ki.kb/target/kb-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
+java -Xmx32G -cp com.ibm.research.ai.ki.kb/target/kb-1.0.0-SNAPSHOT-jar-with-dependencies.jar \
 com.ibm.research.ai.ki.kb.conversion.MakeDatasetGeneric -config $config -kb $baseDir/kb -dataset $baseDir/dataset
 
 # show sample of positive context sets
